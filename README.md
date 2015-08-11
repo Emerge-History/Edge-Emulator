ebe5bdd6f51cef8b3e5ac2ba3d261990d2b431a3
rockdragon <moyerock@gmail.com>
2015-08-11 15:29:50 +0800

    support nginx

--- README.md
+++ README.md
@@ -3,6 +3,18 @@ Edge emulator
 
 Usage
 ======
+[1] append a line to /etc/hosts:
 ```
-$ edge app.js
+127.0.0.1 api.wi.fi
 ```
+[2] append contents of nginx/nginx.conf to your nginx.conf
+
+[3] run program
+    * dynamic:
+    ```
+    $ edge -d app.js
+    ```
+    * static:
+    ```
+    $ edge -s tests/
+    ```
--- webEx/server.js
+++ webEx/server.js
@@ -1,13 +1,10 @@
 var express = require('express');
-var Busboy = require('busboy');
 var bodyParser = require('body-parser');
 var app = express();
 var fs = require('fs');
 var _port = "/tmp/fdsock/webex";
+var methods = require('../lib/methods');
 
-app.post("/fd/:id", function (req, res) {
-    PipeToWebFD(req.param('id'), req, res);
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/", function (req, res) {
@@ -25,7 +22,7 @@ app.post("*", function (req, res) {
         d = d.substr(0, d.length - 1);
     }
     console.log('Web EX Call: ' + d);
-    if (!APIManager.APIDict[d]) {
+    if (!global.API_JSON.f[d]) {
         return res.json({
             error: 'Not Found'
         });
@@ -56,64 +53,16 @@ app.post("*", function (req, res) {
                 });
             }
         }, 20000));
-        var mockRPC = {
-            rpc: { remote: 0 },
-            webex: {
-                deviceid: req.header("edge-dev"),
-                userid: req.header("edge-user")
-            },
-            referer: req.header("referer")
-        };
+
         try {
-            APIManager.APIDict[d].apply(mockRPC, params);
+            methods.MethodShell(d, params);
         }
         catch (e) {
             params[params.length - 1](e);
         }
     }
 });
-function PipeToWebFD(id, req, res) {
-    var end = once(function (err, result) {
-        if (err) {
-            res.json({
-                error: err.message ? err.message : err,
-                stack: err.stack ? err.stack : [],
-            });
-        }
-        else {
-            res.json({
-                result: result,
-                success: "1"
-            });
-        }
-    });
-    var target = id;
-    if (!FIFO.all[id]) {
-        return end(new Error("Your FIFO is incorrect - " + target));
-    }
-    var one = 0;
-    var busboy = new Busboy({ headers: req.headers });
-    busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-        console.log('FILE.. - ' + fieldname);
-        file.pipe(fs.createWriteStream("/tmp/fdsock/" + target));
-    });
-    busboy.on('finish', function () {
-        end();
-    });
-    console.log('WEBFD.. - ' + id);
-    req.pipe(busboy);
-}
-function CreateWebFD(cb) {
-    if (this.webex) {
-        FIFO.CreateSource("WEB", "/tmp/fdsock", function (err, result) {
-            if (err)
-                return cb(err);
-            else {
-                return cb(undefined, result);
-            }
-        });
-    }
-}
+
 function Initialize(cb) {
     console.log("Initializing WebEX");
     app.listen(_port, function () {

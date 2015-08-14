a0abc204d069f42f7e4b54d58d5571e2af492baf
rockdragon <moyerock@gmail.com>
2015-08-14 20:05:51 +0800

    support static server CORS.

--- Servers/staticServer.js
+++ Servers/staticServer.js
@@ -6,6 +6,15 @@ var vhost = require('vhost');
 module.exports = function (staticPath) {
     var app = express();
 
+    var allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        next();
+    };
+    app.use(allowCrossDomain);
+
     app.use(logger('dev'));
     app.use(express.static(staticPath));
     return app;
